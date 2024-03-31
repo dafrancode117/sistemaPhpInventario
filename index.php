0b33eefffbedd63d9f9257597b9cf489ca5a7b5b
Daniel Franco Coila Corini <frank117js@gmail.com>
2024-03-31 18:36:20 -0400

    Avance en la restriccion de acceso al usuario no logeado

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-   // * ARCHIVO PRINCIPAL DEL SISTEMA
-   // -> Incluimos las variables de sesion
-   require_once "./inc/sessionStart.php";
+// * ARCHIVO PRINCIPAL DEL SISTEMA
+// -> Incluimos las variables de sesion
+require_once "./inc/sessionStart.php";
 ?>
 
 <!DOCTYPE html>
@@ -14,26 +14,35 @@
 <body>
    <header>
       <?php
-         // -> Condicional que verifica si la variable de tipo get viene definida o no
-         if(!isset($_GET["vista"]) || $_GET["vista"] == ""){ // isset es una funcion que verifica que una variable este definida y no sea null
-            $_GET["vista"] = "login";
+      // -> Condicional que verifica si la variable de tipo get viene definida o no
+      if (!isset($_GET["vista"]) || $_GET["vista"] == "") { // isset es una funcion que verifica que una variable este definida y no sea null
+         $_GET["vista"] = "login";
+      }
+
+      // -> Condicional que verifica que exista una vista que no sea la del "login" ni la de error "404" para acceder al sistema
+      if (is_file("./views/" . $_GET["vista"] . ".php") && $_GET["vista"] != "login" && $_GET["vista"] != "404") { // is_file es un metodo que indica que el directorio es un directorio normal
+
+         // ! -> Cierre de Sesion forzado para los usuarios que no ha iniciado sesion y quieren entrar al sistema modificando la URL
+         // Usamos las veriables de sesion que se crean para validar
+         if ((!isset($_SESSION['id']) || $_SESSION['id'] == "") || (!isset($_SESSION['usuario']) || $_SESSION['usuario'] == "")) {
+            // -> Llamamos al archivo logout del sistema
+            include './views/logout.php';
+            exit(); // detenemos la ejecucion del programa
          }
 
-         // -> Condicional que verifica que exista una vista que no sea la del "login" ni la de error "404" para acceder al sistema
-         if(is_file("./views/" . $_GET["vista"] . ".php") && $_GET["vista"] != "login" && $_GET["vista"] != "404"){ // is_file es un metodo que indica que el directorio es un directorio normal
-            include_once "./inc/navbar.php";
-            include_once "./views/" . $_GET["vista"] . ".php";
-            include_once "./inc/script.php";
-            
-         } else{
+         // Carga de las vistas del sistema
+         include_once "./inc/navbar.php";
+         include_once "./views/" . $_GET["vista"] . ".php";
+         include_once "./inc/script.php";
+      } else {
 
-            // -> Condicional que verifica que la vista si es un 'login' o un 'error 404'
-            if($_GET["vista"] == "login"){
-               include_once "./views/login.php";
-            } else{
-               include_once "./views/404.php";
-            }
+         // -> Condicional que verifica que la vista si es un 'login' o un 'error 404'
+         if ($_GET["vista"] == "login") {
+            include_once "./views/login.php";
+         } else {
+            include_once "./views/404.php";
          }
+      }
       ?>
    </header>
 </body>
--- views/home.php
+++ views/home.php
@@ -1,5 +1,5 @@
 <!-- //* Vista del la pagina de Inicio del Sistema *//  -->
 <div class="container is-flui is-centered">
    <h1 class="title">HOME</h1>
-   <h2 class="subtitle">Bienvenido Usuario!</h2>
+   <h2 class="subtitle">Bienvenido <?php echo $_SESSION['nombre'] . " " . $_SESSION['apellido'] ?>!</h2>
 </div>
